a13790465b350fb7cdb62d25fff4208831eef747
JosuaMnl <josuamnalu@gmail.com>
2023-08-23 16:54:32 +0700

    feat: Add functional login feature

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -9,15 +9,28 @@
                         <img src="{{ asset('assets/images/footer-logo.png') }}" alt="Logo">
                     </a>
                     <div class="card mb-5">
-                        <form action="" method="post">
+                        <form action="{{ route('auth.login.login') }}" method="POST">
+                            @csrf
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="name@example.com"
-                                    autocomplete="off" autofocus>
+                                <input type="email"
+                                    class="form-control @error('email') is-invalid @enderror @error('credentials') is-invalid @enderror"
+                                    id="email" name="email" placeholder="name@example.com" autocomplete="off"
+                                    autofocus value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                                @error('credentials')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" placeholder="****************">
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="password" name="password" placeholder="****************">
+                                @error('password')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3 d-grid">
                                 <button type="submit" class="btn btn-primary rounded-2">Log In</button>
